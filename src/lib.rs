e047a99361e9a4f08a4838a79f2a9f604a88a66e
thanhtan541 <thanhtan004100@gmail.com>
2024-07-24 10:44:18 +0700

    feat(dep): add tailwindcss

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,7 @@ pub fn App() -> impl IntoView {
         // injects metadata in the <head> of the page
         <Meta charset="UTF-8"/>
         <Meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <Stylesheet id="leptos" href="/style/output.css"/>
 
         <Router>
             <Routes>
