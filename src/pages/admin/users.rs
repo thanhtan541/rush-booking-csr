11b29eb3302929918cc5257cb9b8d0d76e79dee8
thanhtan541 <thanhtan004100@gmail.com>
2024-07-29 20:53:13 +0700

    feat: add highlight for selected item

--- src/pages/admin/users.rs
+++ src/pages/admin/users.rs
@@ -5,18 +5,23 @@ use leptos_router::*;
 #[component]
 pub fn Users() -> impl IntoView {
     let rows = 10;
+    let (selected, set_selected) = create_signal(0);
     view! {
         <LayoutWithAuth>
+            <p>USER LIST</p>
             <div class="grid grid-cols-2 gap-4 mb-4">
-                 <div class="flex items-center justify-center h-60 rounded bg-gray-50 dark:bg-gray-800">
-                    <p>User lists</p>
-                    <ul>
+                // List view
+                 <div class="flex flex-col border-2 border-gray-200 border-dashed rounded dark:bg-gray-800">
                     {(1..rows)
-                        .map(|n| view! { <li>User number: {n}</li>})
+                        .map(|n| view! {
+                            <div class=("bg-gray-100", move || selected() == n)>
+                                <ListItem id=n title=n.to_string() href=n.to_string() on_click=set_selected />
+                            </div>
+                        })
                         .collect_view()}
-                    </ul>
                  </div>
-                 <div class="flex items-center justify-center h-60 rounded bg-gray-50 dark:bg-gray-800">
+                // Item detail
+                 <div class="flex items-center justify-center border-2 border-gray-200 border-dashed rounded bg-gray-50 dark:bg-gray-800">
                     <Outlet/>
                  </div>
             </div>
@@ -25,8 +30,35 @@ pub fn Users() -> impl IntoView {
 }
 
 #[component]
+fn ListItem(
+    /// Id of the listitem
+    id: i32,
+    /// Title of the listitem
+    title: String,
+    /// Link to related resource, e.g: http://domain/path/to/resource
+    href: String,
+    on_click: WriteSignal<i32>,
+) -> impl IntoView {
+    let class = "flex items-center justify-center p-2 text-gray-900 rounded-lg dark:text-hite hover:bg-gray-100 dark:hover:bg-gray-700";
+    view! {
+       <A href={href} class={class} on:click=move |_| on_click.update(|value| *value = id) >
+        <div>
+            <p class="text-xl">{id}</p>
+            <p>{title} bed room</p>
+        </div>
+       </A>
+    }
+}
+
+#[derive(Params, PartialEq)]
+struct UserParams {
+    id: usize,
+}
+#[component]
 pub fn UserProfile() -> impl IntoView {
-    let id = 1;
+    let params = use_params::<UserParams>();
+    let id =
+        move || params.with(|params| params.as_ref().map(|params| params.id).unwrap_or_default());
     view! {
         <p>User id: {id}</p>
     }
